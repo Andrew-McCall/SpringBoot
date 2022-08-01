29656af15a30a0ff0b86a6e15b2735b6df32f4e2
Andrew-McCall <amccall@qa.com>
2022-08-01 14:09:06 +0100

    ScanComponent and profile

--- src/main/java/com/qa/demo/QaSpringApplication.java
+++ src/main/java/com/qa/demo/QaSpringApplication.java
@@ -3,12 +3,16 @@ package com.qa.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value="com.qa.beans")
 public class QaSpringApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(QaSpringApplication.class, args);
+
+		new Profile(0, null);
 		
 	    Object byName = context.getBean("greeting");
 	    //String byType = context.getBean(String.class);

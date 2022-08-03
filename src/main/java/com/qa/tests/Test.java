24f162e93b5a0aa3f49d971b573ee0de3ab4df45
Andrew-McCall <amccall@qa.com>
2022-08-03 14:31:25 +0100

    Init Dataset

---
+++ src/main/java/com/qa/tests/Test.java
@@ -0,0 +1,9 @@
+package com.qa.tests;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/resources/PersonData.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNT (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('amccall@qa.com', 'Andrew', 'McCall');
+INSERT INTO ACCOUNT (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('notmccall@qa.com', 'Drew', 'Call');
+INSERT INTO ACCOUNT (EMAIL, FIRST_NAME, LAST_NAME) VALUES ('david@qa.com', 'David', 'McCall');

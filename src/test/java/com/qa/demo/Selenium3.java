92f9d75f361aa4680fd20174941d3c49c7a64cb2
Andrew-McCall <amccall@qa.com>
2022-08-18 14:38:41 +0100

    Thrid selenium test

--- src/test/java/com/qa/demo/Selenium3.java
+++ src/test/java/com/qa/demo/Selenium3.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,7 +16,7 @@ import com.qa.runner.QaSpringApplication;
 
 @SpringBootTest(classes = { QaSpringApplication.class })
 @ActiveProfiles("dev")
-public class Selenium {
+public class Selenium3 {
 
 	private static WebDriver driver;
 
@@ -23,19 +24,21 @@ public class Selenium {
 	public static void setup() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		driver = new ChromeDriver();
-		System.out.println("driver");
-	}
-
-	@AfterAll
-	public static void tearDown() {
-		driver.quit();
 	}
 
 	@Test
-	public void index() {
-		driver.get("http://localhost:8090/index.html");
-		assertEquals(driver.getTitle(), ("Index"));
-		assertEquals(driver.findElement(By.xpath("//p")).getText(), ("Andrew, McCall"));
+	public void homePage() throws InterruptedException {
+
+		driver.get("http://localhost:8090/home.html");
+		assertEquals("Home Page", driver.getTitle());
+		WebElement element = driver.findElement(By.xpath("/html/body/h1"));
+		assertEquals("Hello !", element.getText());
+
+	}
+
+	@AfterAll
+	public static void cleanup() {
+		driver.close();
 	}
 
 }
